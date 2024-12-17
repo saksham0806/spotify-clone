7ed1a220f739e8e5c38ad84c93732df7f04ff6fa
merdain0806 <sakshamsingh0806alt@gmail.com>
2024-12-17 11:36:01 +0530

    updated playbar and added songs reader

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 console.log("JavaScript is running")
+
+async function  main(){
+    let a = await fetch("http://127.0.0.1:3000/songs/")
+    let response = await a.text()
+    console.log(response)
+}
+
+main();
