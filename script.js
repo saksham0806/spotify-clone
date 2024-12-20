b41d34c738624537b22d10f3ca963b97c6b5b652
merdain0806 <sakshamsingh0806alt@gmail.com>
2024-12-20 18:11:40 +0530

    added songbar functionality

--- script.js
+++ script.js
@@ -10,15 +10,20 @@ async function getsongs(){
     for(let i=0;i<a.length;i++){
         let element = a[i];
         if(element.href.endsWith(".mp3")){
-            songs.push(element.href.split("/songs/")[1].split(".mp3")[0].replaceAll("-"," "));
+            songs.push(element.href.split("/songs/")[1]);
         }
     }
 
     return songs;
 }
 
+async function playmusic(track){
+    let audio = new Audio("\\songs\\"+track);
+    console.log(audio.duration);
+    audio.play;
+}
 
-async function main(){
+async function leftbarsongslist(){
     let songs = await getsongs();
     let songul = document.querySelector(".songsnames").getElementsByTagName("ul")[0]
     for(let i=0;i<songs.length;i++){
@@ -26,13 +31,30 @@ async function main(){
         tul.innerHTML = `<div class="song">
                             <img src="images/Music.svg" alt="">
                             <div class="songnameart">
-                                <div class="songname">${songs[i]}</div>
+                                <div class="songname">${songs[i].split(".mp3")[0].replaceAll("%20", " ")}</div>
                                 <div class="artistname">Saksham</div>
                             </div>
-                            <img src="images/play.svg" alt="">
+                            <img src="images/playbarplay.svg" alt="">
                         </div>`;
         songul.appendChild(tul)
     }
+
+    let currentsongs;
+    Array.from(document.querySelector(".songsnames").getElementsByTagName("li")).forEach((song)=>{
+        song.addEventListener("click",e=>{
+            playmusic(song.getElementsByClassName("songname")[0].innerHTML.trim());
+            console.log("clicked")
+        })
+        console.log(song.getElementsByClassName("songname")[0].innerHTML);
+    })
+}
+
+leftbarsongslist();
+
+async function main() {
+
+
 }
 
 main();
+
